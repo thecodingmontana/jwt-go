9f2be22f143bb51c8ae2aef65759cb588f6f9ab5
thecodingmontana <dev.humblechris254@gmail.com>
2024-12-26 21:01:02 +0300

    🧹 chore(app): Added cors

--- cmd/jwt-go/main.go
+++ cmd/jwt-go/main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 	"github.com/thecodingmontana/jwt-go/internal/routes"
 )
@@ -28,6 +29,16 @@ func main() {
 
 	// Middlewares
 	router.Use(middleware.Logger)
+	router.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 
 	routes.RegisterRoutes(router)
 
